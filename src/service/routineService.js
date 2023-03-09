a6f6b55d4639c0473af66feacd580f9d34b6055a
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-08 22:44:13 -0300

    Routine service, create.

--- src/service/routineService.js
+++ src/service/routineService.js
@@ -1,7 +1,19 @@
+const { Routine } = require('../db.js')
 
 class RoutineService{
 
+async createRoutine(name, timeForWeek){
+    try {
+        createdRoutine = await Routine.create({name, timeForWeek});
+        return createdRoutine
+    
+    } catch (error) {
+        return error
+    }
 
+   
+    
+}
 
 }
 
